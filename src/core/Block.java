b5ef0a66fb71979c0ed60d852f227abed2e20424
user <user@LAPTOP-4FNES964>
2019-06-13 14:58:50 +0900

    update : Block/Transaction class update
    ** 코인 보내기/이전 Hash값 저장

---
+++ src/core/Block.java
@@ -0,0 +1,78 @@
+package core;
+
+import java.util.ArrayList;
+
+import util.Util;
+
+public class Block {
+	
+	private int blockID;
+	private int nonce;
+	private String previousBlockHash;
+	private ArrayList transactionList;
+	
+	public int getBlockID() {
+		return blockID;
+	}
+	public void setBlockID(int blockID) {
+		this.blockID = blockID;
+	}
+	public int getNonce() {
+		return nonce;
+	}
+	public void setNonce(int nonce) {
+		this.nonce = nonce;
+	}
+	public String getPreviousBlockHash() {
+		return previousBlockHash;
+	}
+	public void setPreviousBlockHash(String previousBlockHash) {
+		this.previousBlockHash = previousBlockHash;
+	}
+	
+	public Block(int blockID, String previousBlockHash, int nonce, ArrayList transactionList) {
+		this.blockID = blockID;
+		this.previousBlockHash = previousBlockHash;
+		this.nonce = nonce;
+		this.transactionList = transactionList;
+	}
+	
+	public void addTransaction(Transaction transaction) {
+		transactionList.add(transaction);
+	}
+	
+	public void showInformation() {
+		System.out.println("-------------------------------------------------");
+		System.out.println("Block Num : " + getBlockID());
+		System.out.println("pre Hash : " + getPreviousBlockHash());
+		System.out.println("Mining  variable : " + getNonce());
+		System.out.println("Transaction amount : " + transactionList.size() + "개");
+		for(int i=0; i<transactionList.size(); i++) {
+			System.out.println(transactionList.get(i).getInformation());
+		}
+		System.out.println("Block Hash : " + getBlockHash());
+		System.out.println("-------------------------------------------------");
+	}
+	
+	public String getBlockHash() {
+		String transactionInformations = "";
+		for(int i=0; i<transactionList.size(); i++) {
+			transactionInformations += transactionList.get(i).getInformation();
+		}
+		return Util.getHash(nonce + transactionInformations + previousBlockHash);
+	}
+	
+	public void mine() {
+		while(true) {
+			if(getBlockHash().substring(0, 4).equals("0000")){
+				System.out.println(blockID + "번째 블록 채굴에 성공했습니다.");
+				break;
+			}
+			nonce++;
+		}
+	}
+	
+	
+	
+	
+}
--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.ArrayList;
+
 import util.Util;
 
 public class BlockChainStarter {
@@ -12,16 +14,27 @@ public class BlockChainStarter {
 		 * BlockChain 실행기
 		 * 
 		 * **/
+
+		Block block1 = new Block(1, null, 0, new ArrayList());
+		block1.mine();
+		block1.showInformation();
 		
-		int nonce = 0;
-		while(true) {
-			if(Util.getHash(nonce + "").substring(0, 6).equals("000000")) {
-				System.out.println("정답 : " + nonce);
-				
-				break;
-			}
-			nonce++;
-		}
+		Block block2 = new Block(2, block1.getBlockHash(), 0, new ArrayList());
+		block2.addTransaction(new Transaction("나동빈", "박한울", 1.5));
+		block2.addTransaction(new Transaction("이태일", "박한울", 0.7));
+		block2.mine();
+		block2.showInformation();
+		
+		Block block3 = new Block(3, block2.getBlockHash(), 0, new ArrayList());
+		block3.addTransaction(new Transaction("강종구", "이상국", 8.2));
+		block3.addTransaction(new Transaction("박한울", "나동빈", 0.4));
+		block3.mine();
+		block3.showInformation();
+		
+		Block block4 = new Block(4, block3.getBlockHash(), 0, new ArrayList());
+		block4.addTransaction(new Transaction("이상국", "강종구", 0.1));
+		block4.mine();
+		block4.showInformation();
 		
 	}
 
