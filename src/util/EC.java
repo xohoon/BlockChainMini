b1da030f045db5b2da32dfede8f28d387c87e169
user <user@LAPTOP-4FNES964>
2019-06-13 16:48:28 +0900

    update : EC class update

--- src/util/EC.java
+++ src/util/EC.java
@@ -1,14 +1,26 @@
 package util;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import org.bouncycastle.util.encoders.Base64;
+
+import jdk.nashorn.internal.runtime.regexp.JoniRegExp.Factory;
 
 public class EC {
 	
@@ -44,9 +56,79 @@ public class EC {
 		Pem pemFile = new Pem(key, description);
 		pemFile.write(filename);
 		System.out.println(String.format("EC 암호키 %s을(를) %s 파일로 보냈습니다.", description, filename));
+
+	}
+	
+	// 문자열 형태의 인증서에서 개인키를 추출하는 함수
+	
+	public PrivateKey readPrivateKeyFromPemFile(String privateKeyName)
+			throws FileNotFoundException, IOException,
+			NoSuchAlgorithmException, InvalidKeySpecException {
+		
+		String data = readString(privateKeyName);
+		System.out.println("EC 개인키를 " + privateKeyName + "로부터 불러왔습니다");
+		System.out.println(data);
+		
+		// 불필요한 설명 구문을 제거
+		data = data.replaceAll("-----BEGIN EC PRIVATE KEY-----", "");
+		data = data.replaceAll("-----END EC PRIVATE KEY-----", "");
+		
+		// PEM 파일은 Base64로 인코딩 되어있으므로 디코딩해서 읽을 수 있도록 한다
+		byte[] decoded = Base64.decode(data);
+		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+		KeyFactory factory = KeyFactory.getInstance("ECDSA");
+		PrivateKey privateKey = factory.generatePrivate(spec);
+		
+		return privateKey;
+	}
+	
+	// 문자열 형태의 이증서에서 공개키를 추출하는 함수
+	public PublicKey readPublicKeyFromPemFile(String publicKeyName) throws 
+	FileNotFoundException, IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+		
+		String data = readString(publicKeyName);
+		System.out.println("EC 개인키를" + publicKeyName + "로부터 불러왔습니다.");
+		System.out.println(data);
+		
+		// 불필요한 설명 구문을 제거합니다.
+		data = data.replaceAll("-----BEGIN EC PUBLIC KEY-----", "");
+		data = data.replaceAll("-----END EC PUBLIC KEY-----", "");
+		
+		// PEM 파일은 Base64로 인코딩 되어있으므로 디코딩해서 읽을 수 있도록 한다
+		byte[] decode = Base64.decode(data);
+		X509EncodedKeySpec spec = new X509EncodedKeySpec(decode);
+		KeyFactory factory = KeyFactory.getInstance("ECDSA");
+		PublicKey publicKey = factory.generatePublic(spec);
+		
+		return publicKey;
+	}
+	
+	// 특정한 파일에 작성되어 잇는 문자열을 그대로 읽어오는 함수입니다.
+	private String readString(String filename) throws FileNotFoundException, IOException {
+		
+		String pem = "";
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		String line;
+		while((line = br.readLine()) != null)
+			pem += line + "\n";
+		br.close();
+		
+		return pem;
 		
 	}
 	
 	
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
